ba7f3db9e51debc3caf4753540b43ec967d51f8f
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-12-27 16:18:53 -0600

    Simple behavior works

--- index.js
+++ index.js
@@ -2,31 +2,39 @@ const fs = require("fs")
 const http = require("http")
 
 const config = JSON.parse(fs.readFileSync("./config.json"))
-const routes = JSON.parse(fs.readFileSync("./routes.json"))
+
+function combine(p1, p2)
+{
+    return p1+"/"+p2
+}
 
 function findRoute(url)
 {
-    let route 
-    let subs = url.substring(1).split("/")
-    // console.log(subs)
-
-    return "."+url
+    url = url.replace("..", "")
+    const withRoot = combine(config['root-dir'], url)
+    if (url.indexOf(".") != -1)
+    {
+        // Is file
+        return withRoot
+    } else if (fs.existsSync(withRoot+".htm")) {
+        return withRoot+".htm"
+    } else if (fs.existsSync(withRoot+".html")) {
+        return withRoot+".html"
+    } else {
+        return combine(withRoot, config["dir-page"])
+    }
 }
 
 const server = http.createServer((req, res) => {
     const route = findRoute(req.url)
 
-    console.log(req.url)
-    console.log(route)
-    console.log(route.split(".")[1])
-
     if (fs.existsSync(route))
     {
-        res.writeHead(200, { 'Content-Type': config["response-types"][route.split(".")[1]] })
+        res.writeHead(200, { 'Content-Type': config["response-types"][route.substring(route.lastIndexOf(".")+1)] })
         res.write(fs.readFileSync(route))
     } else {
         res.writeHead(404)
-        res.write(fs.readFileSync(config["404Page"]))
+        res.write(fs.readFileSync(combine(config["root-dir"], config["404Page"])))
     }
 
     res.end()
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Simple Static Delivery Server
+This is a very simple static delivery server
+
+**Note**: It is probably very unsecure ngl
